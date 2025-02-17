8656244d48fb11115b402c511e020150b5a4fa1c
toongav <toon_gav2005@yahoo.co.uk>
2025-02-17 16:14:30 +0000

    Update uservar.py

--- repo/plugin.program.toonwiz/uservar.py
+++ repo/plugin.program.toonwiz/uservar.py
@@ -77,7 +77,7 @@ THEME5 = u'[COLOR {color1}]Current Theme:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT = 'Yes'
 # You can add \n to do line breaks
-#CONTACT = 'Thank you for choosing Ragnarok.\n\nContact us me on Discord: Ragnar Lothbrok#1948'
+CONTACT = 'Thank you for choosing ToonWiz.\n\n'
 # Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON = os.path.join(ART, 'qricon.png')
 CONTACTFANART = 'http://'
